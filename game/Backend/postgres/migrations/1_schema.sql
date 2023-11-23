e09a8d4b1b3b4db1708612759d7900323537079c
Pedro Lima <phlimas@outlook.com>
2023-11-23 10:44:30 -0300

    Fazendo o banco popular e criar tabela sem problema

---
+++ game/Backend/postgres/migrations/1_schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100)
+);
---
+++ game/Backend/postgres/migrations/2_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (name, email) VALUES ('John Doe', 'john@example.com');
+INSERT INTO users (name, email) VALUES ('Jane Doe', 'jane@example.com');
