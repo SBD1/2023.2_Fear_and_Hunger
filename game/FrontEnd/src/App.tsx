9d0d2f6ab0aa490de1e586ca12a1322f36bbc10a
Pedro Lima <phlimas@outlook.com>
2023-12-02 22:14:10 -0300

    Ajuste do docker e ajuste das rotas do front e backend
    Co-authored-by: Alexandre <alexandrebeck2003@gmail.com>
    Co-authored-by: Gabriela <gabriela.leemoss@gmail.com>
    Co-authored-by: Matheus <matheus.fsousa11@gmail.com>
    Co-authored-by: Pedro <pedrolimas@outlook.com>

--- game/FrontEnd/src/App.tsx
+++ game/FrontEnd/src/App.tsx
@@ -1,46 +1,38 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import api from './api'
+import api from "./api";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Game from "./pages/Game";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState("")
+  // const [count, setCount] = useState(0);
+  const [data, setData] = useState("");
 
   const testApi = async () => {
-    const {data} = await api.get("/")
-    setData(data.message);
-  }
+    const { data } = await api.get("/");
+    setData(data.message);    
+  };
 
   useEffect(() => {
-    testApi()
-  }, [])
-  
+    testApi();
+  }, []);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/game",
+      element: <Game/>,
+    },
+  ]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>      
-      <p>{data}</p>
+      <RouterProvider router={router} />
+      <p className="api">{data}</p>
     </>
   );
 }
