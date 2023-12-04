ddedf029b97430dac7bf63a999885093f6ff47e0
gatotabaco <matheus.fsousa11@gmail.com>
2023-12-04 19:16:33 -0300

    adicionando os creat table

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -1,245 +1,252 @@
--- Tabela Personagem
-CREATE TABLE
-  Personagem (
-    idPersonagem INT PRIMARY KEY AUTO_INCREMENT,
-    alma INT DEFAULT 0,
-    local INT UNIQUE,
-    tipoP INT NOT NULL
+CREATE TABLE Regiao (
+    idRegiao SERIAL PRIMARY KEY,
+    nomeR VARCHAR(25) NOT NULL,
+    descricao TEXT DEFAULT '',
+	tranca bool DEFAULT FALSE
+);
+
+CREATE TABLE Local (
+    idLocal SERIAL,
+	idRegiao SERIAL REFERENCES Regiao(idRegiao),
+    nomeL VARCHAR(25) NOT NULL,
+    imgTexto TEXT,
+	CONSTRAINT pk_CE primary key(idLocal, idRegiao)
+);
+
+CREATE TABLE Item (
+    idItem SERIAL PRIMARY KEY,
+	  tipoItem TEXT NOT NULL
+);
+
+CREATE TABLE Acessorio(
+    idItem SERIAL PRIMARY KEY REFERENCES Item (idItem),
+    adAtqM int DEFAULT 0,
+    adAgil int DEFAULT 0,
+    adDefM int DEFAULT 0,
+    adDef int default 0, 
+    adAtqF int default 0,
+    valor int default 0,
+    peso int NOT NULL,
+    nome VARCHAR(25) NOT NULL,
+    descricao text DEFAULT ''
+);
+
+CREATE TABLE Armadura(
+    idItem SERIAL PRIMARY KEY REFERENCES Item (idItem),
+    adAgil int DEFAULT 0,
+    adDefM int DEFAULT 0,
+    adDef int default 0, 
+    valor int default 0,
+    peso int NOT NULL,
+    nome VARCHAR(25) NOT NULL,
+    descricao text DEFAULT ''
+);
+
+CREATE TABLE Legivel(
+    idItem SERIAL PRIMARY KEY REFERENCES Item (idItem),
+    conteudo TEXT NOT NULL,
+    valor int default 0,
+    peso int NOT NULL,
+    nome VARCHAR(25) NOT NULL,
+    descricao text DEFAULT ''
+);
+
+CREATE TABLE Arma(
+    idItem SERIAL PRIMARY KEY REFERENCES Item (idItem),
+    adAtqM int DEFAULT 0,
+    adDef int default 0, 
+    adAtqF int default 0,
+    valor int default 0,
+    peso int NOT NULL,
+    nome VARCHAR(25) NOT NULL,
+    descricao text DEFAULT ''
+);
+
+CREATE TABLE Consumivel(
+    idItem SERIAL PRIMARY KEY REFERENCES Item (idItem),
+    conteudo TEXT DEFAULT '',
+    adMente int default 0,
+    adDefM int default 0, 
+    adAgil int default 0,
+    adHp int default 0,
+    valor int default 0,
+    peso int NOT NULL,
+    nome VARCHAR(25) NOT NULL,
+    descricao text DEFAULT ''
+);
+
+CREATE TABLE Chave(
+    idItem SERIAL PRIMARY KEY REFERENCES Item (idItem),
+    Regiao SERIAL REFERENCES Regiao(idRegial) NOT NULL,
+    valor int default 0,
+    peso int NOT NULL,
+    nome VARCHAR(25) NOT NULL,
+    descricao text DEFAULT ''
+);
+
+CREATE TABLE Personagem(
+  idPersonagem SERIAL PRIMARY KEY,
+	local SERIAL,
+	regiao SERIAL,
+  FOREIGN KEY(local, regiao) REFERENCES Local(idLocal, idRegiao),
+  tipoP VARCHAR(3) NOT NULL
+);
+
+CREATE TABLE PersonagemJogavel(
+  idPersonagem SERIAL PRIMARY KEY REFERENCES Personagem (idPersonagem),
+  acessorio1 SERIAL REFERENCES Acessorio (idItem),
+  acessorio2 SERIAL REFERENCES Acessorio (idItem),
+  armadura SERIAL REFERENCES Armadura (idItem),
+  arma SERIAL REFERENCES Arma (idItem),
+  menteAtual INT NOT NULL,
+  menteMax INT NOT NULL,
+  hpAtual INT NOT NULL,
+  hpMax INT NOT NULL,
+  atq INT DEFAULT 0,
+  agilidade INT DEFAULT 10,
+  defesaM int DEFAULT 0,
+  defesa INT DEFAULT 0,
+  atqM INT Default 0,
+  nome text NOT NULL
+);
+
+CREATE TABLE PersonagemNaoJogavel(
+  idPersonagem SERIAL PRIMARY KEY REFERENCES Personagem(idPersonagem),
+  tipoPnj VARCHAR(1) NOT NULL
   );
 
--- Tabela Loot
-CREATE TABLE
-  Loot (
-    local INT FOREIGN KEY REFERENCES Local (idLocal),
-    item INT FOREIGN KEY REFERENCES Item (idItem)
-  );
+CREATE TABLE Lojista(
+  idPersonagem SERIAL PRIMARY KEY REFERENCES PersonagemNaoJogavel(idPersonagem),
+	local SERIAL,
+	regiao SERIAL,
+  FOREIGN KEY(local, regiao) REFERENCES Local(idLocal, idRegiao),
+  fala TEXT DEFAULT '',
+  imgTexto TEXT DEFAULT '',
+  descricao TEXT DEFAULT '',
+  atq int DEFAULT 0,
+  agilidade INT DEFAULT 15,
+  defesaM INT DEFAULT 0,
+  defesa INT DEFAULT 0,
+  atqM INT DEFAULT 0,
+  nome TEXT NOT NULL
+);
 
--- Tabela Coletadas
-CREATE TABLE
-  Coletadas (
-    personagem INT FOREIGN KEY REFERENCES PersonagemJogavel (idPersonagem),
-    alma VARCHAR(255) FOREIGN KEY REFERENCES Alma (nome),
-  );
+CREATE TABLE Inimigo(
+  idPersonagem SERIAL PRIMARY KEY REFERENCES PersonagemNaoJogavel(idPersonagem),
+  fala TEXT DEFAULT '',
+  imgTexto TEXT DEFAULT '',
+  descricao TEXT DEFAULT '',
+  atq int DEFAULT 0,
+  agilidade INT DEFAULT 15,
+  defesaM INT DEFAULT 0,
+  defesa INT DEFAULT 0,
+  atqM INT DEFAULT 0,
+  nome TEXT NOT NULL
+);
 
--- Tabela Ataque
-CREATE TABLE
-  Ataque (
-    idAtaque INT PRIMARY KEY AUTO_INCREMENT,
-    tipoAtaque INT NOT NULL,
-    descrição VARCHAR(255) DEFAULT '',
-    idHabilidade INT FOREIGN KEY REFERENCES Habilidade (idHabilidade),
-    danoFisico INT DEFAULT 0,
-    danoMagico INT DEFAULT 0
-  );
+CREATE TABLE Alma(
+  nome VARCHAR(25) PRIMARY KEY,
+  idPersonagem SERIAL REFERENCES PersonagemJogavel(idPersonagem),
+  descricao TEXT DEFAULT ''
+);
 
--- Tabela Item
-CREATE TABLE
-  Item (
-    idItem INT PRIMARY KEY AUTO_INCREMENT,
-    tipo VARCHAR(255) NOT NULL,
-    idInventario INT FOREIGN KEY REFERENCES Inventario (idInventario),
-    lojista INT FOREIGN KEY REFERENCES Lojista (idLojista)
-  );
+ALTER TABLE personagem ADD COLUMN alma TEXT REFERENCES Alma (nome);
 
--- Tabela Armadura
-CREATE TABLE
-  Armadura (
-    idItem INT FOREIGN KEY REFERENCES Item (idItem),
-    defesa INT DEFAULT 0,
-    defesaMagica INT DEFAULT 0,
-    agilidade INT DEFAULT 0,
-    descrição VARCHAR(255) DEFAULT '',
-    valor INT DEFAULT 0,
-    nome VARCHAR(255) NOT NULL
-  );
+CREATE TABLE Parte(
+  idParte SERIAL,
+  idPersonagem INT,
+  hpMax INT NOT NULL,
+  probAcerto INT NOT NULL,
+  hpAtual INT NOT NULL,
+  tipoParte TEXT NOT NULL,
+  CONSTRAINT pk_CE PRIMARY KEY(idParte, idPersonagem),
+  FOREIGN KEY (idPersonagem) REFERENCES personagem (idPersonagem)
+);
 
--- Tabela Local
-CREATE TABLE
-  Local (idLocal INT PRIMARY KEY AUTO_INCREMENT);
+CREATE TABLE InstanciaInimigo(
+  idInst SERIAL,
+  idPersonagem SERIAL,
+  idLocal Serial,
+  regiao SERIAL,
+  PRIMARY KEY (idInst, idPersonagem),
+  FOREIGN KEY (idPersonagem) REFERENCES personagem (idPersonagem),
+  FOREIGN KEY (idLocal, regiao) REFERENCES Local(idLocal, idRegiao)
+);
 
--- Tabela Estoque
-CREATE TABLE
-  Estoque (
-    idLojista INT FOREIGN KEY REFERENCES Lojista (idLojista),
-    idItem INT FOREIGN KEY REFERENCES Item (idItem)
-  );
+CREATE TABLE InstanciaItem(
+  idInst SERIAL,
+  idItem SERIAL,
+  idLocal SERIAL,
+  idRegiao SERIAL,
+  PRIMARY KEY(idInst, idItem),
+  FOREIGN KEY (idItem) REFERENCES Item (idItem),
+  FOREIGN KEY (idLocal, idRegiao) REFERENCES Local (idLocal, idRegiao)
+);
 
--- Tabela Arma
-CREATE TABLE
-  Arma (
-    idItem INT FOREIGN KEY REFERENCES Item (idItem),
-    ataqueFisico INT DEFAULT 0,
-    ataqueMagico INT DEFAULT 0,
-    descrição VARCHAR(255) DEFAULT '',
-    valor INT DEFAULT 0,
-    nome VARCHAR(255) NOT NULL
-  );
+CREATE TABLE Vende(
+  idLojista SERIAL,
+  idInstItem SERIAL,
+	idItem SERIAL,
+  PRIMARY KEY (idLojista, idInstItem),
+  FOREIGN KEY (idLojista) REFERENCES Lojista (idPersonagem),
+  FOREIGN KEY (idInstItem, idItem) REFERENCES InstanciaItem (idinst, iditem)
+);
 
--- Tabela Acessorio
-CREATE TABLE
-  Acessorio (
-    idItem INT FOREIGN KEY REFERENCES Item (idItem),
-    defesa INT DEFAULT 0,
-    defesaMagica INT DEFAULT 0,
-    agilidade INT DEFAULT 0,
-    ataqueFisico INT DEFAULT 0,
-    ataqueMagico INT DEFAULT 0,
-    descrição VARCHAR(255) DEFAULT '',
-    valor INT DEFAULT 0,
-    nome VARCHAR(255) NOT NULL
-  );
+CREATE TABLE Habilidade(
+  idHabilidade SERIAL PRIMARY KEY,
+  alma TEXT REFERENCES Alma (nome),
+  tipoHab VARCHAR(1) NOT NULL
+);
 
--- Tabela Lojista
-CREATE TABLE
-  Lojista (
-    idLojista INT PRIMARY KEY AUTO_INCREMENT,
-    idInventario INT FOREIGN KEY REFERENCES Inventario (idInventario) agilidade INT DEFAULT 0,
-    defesaMagica INT DEFAULT 0,
-    defesa INT DEFAULT 0,
-    ataque INT DEFAULT 0,
-    nome VARCHAR(255) NOT NULL
-  );
+CREATE TABLE Passiva(
+  idHabilidade SERIAL PRIMARY KEY REFERENCES Habilidade (idHabilidade),
+  adAtqM INT DEFAULT 0,
+  adDefM INT DEFAULT 0,
+  adMenteMax INT DEFAULT 0,
+  adAgil INT DEFAULT 0,
+  adAtqF INT DEFAULT 0,
+  adDef INT DEFAULT 0,
+  adHpMax INT DEFAULT 0,
+  nome TEXT NOT NULL,
+  custo int DEFAULT 0,
+  descricao TEXT DEFAULT ''
+);
 
--- Tabela Personagem Não Jogável
-CREATE TABLE
-  PNJ (
-    idPersonagem INT FOREIGN KEY REFERENCES Personagem (idPersonagem),
-    tipoPnj NOT NULL
-  );
+CREATE TABLE Ataque(
+  idHabilidade SERIAL PRIMARY KEY REFERENCES Habilidade (idHabilidade),
+  danoFisico INT DEFAULT 0,
+  danoMagico INT DEFAULT 0,
+  nome TEXT NOT NULL,
+  custo int DEFAULT 0,
+  descricao TEXT DEFAULT ''
+);
 
--- Tabela Inimigo
-CREATE TABLE
-  Inimigo (
-    idPersonagem INT PRIMARY KEY AUTO_INCREMENT,
-    defesa INT DEFAULT 0,
-    agilidade INT DEFAULT 0,
-    ataque INT DEFAULT 0,
-    nome VARCHAR(255) NOT NULL,
-    defesaMagica INT DEFAULT 0
-  );
+CREATE TABLE Suporte(
+  idHabilidade SERIAL PRIMARY KEY REFERENCES Habilidade (idHabilidade),
+  adAtqM INT DEFAULT 0,
+  adDefM INT DEFAULT 0,
+  adAgil INT DEFAULT 0,
+  adHp INT DEFAULT 0,
+  nome TEXT NOT NULL,
+  custo int DEFAULT 0,
+  descricao TEXT DEFAULT ''
+);
 
--- Tabela Legivel
-CREATE TABLE
-  Legivel (
-    idItem INT PRIMARY KEY AUTO_INCREMENT,
-    conteúdo VARCHAR(255) DEFAULT '',
-    valor INT DEFAULT 0,
-    descrição VARCHAR(255) DEFAULT '',
-    nome VARCHAR(255) NOT NULL
-  );
+CREATE TABLE Inventario(
+  idInventario SERIAL,
+  idPersonagem SERIAL,
+  PRIMARY KEY(idInventario, idPersonagem),
+  FOREIGN KEY (idPersonagem) REFERENCES Personagem(idPersonagem),
+  capTotal INT NOT NULL,
+  capAtual INT NOT NULL,
+  dinMax INT NOT NULL,
+  dinAtual INT NOT NULL
+);
 
--- Tabela Consumivel
-CREATE TABLE
-  Consumivel (
-    idItem INT PRIMARY KEY AUTO_INCREMENT,
-    adHp INT NOT NULL,
-    adMente INT NOT NULL,
-    descrição VARCHAR(255) DEFAULT '',
-    valor INT DEFAULT 0,
-    nome VARCHAR(255) NOT NULL
-  );
-
--- Tabela Inventario
-CREATE TABLE
-  Inventario (
-    idInventario INT PRIMARY KEY AUTO_INCREMENT,
-    idPersonagem INT FOREIGN KEY REFERENCES Personagem (idPersonagem)
-  );
-
--- Tabela Alma
-CREATE TABLE
-  Alma (
-    nome VARCHAR(255) PRIMARY KEY,
-    personagem INT FOREIGN KEY REFERENCES Personagem (idPersonagem)
-  );
-
--- Tabela Personagem Jogável
-CREATE TABLE
-  PersonagemJogavel (
-    corpo INT NOT NULL,
-    mente INT NOT NULL,
-    defesa INT NOT NULL,
-    agilidade INT NOT NULL,
-    ataque INT NOT NULL,
-    nome VARCHAR(255) NOT NULL,
-    defesaMagica INT NOT NULL,
-    idPersonagem INT FOREIGN KEY REFERENCES Personagem (idPersonagem),
-    acessorio1 INT FOREIGN KEY REFERENCES Item (idItem),
-    acessorio2 INT FOREIGN KEY REFERENCES Item (idItem),
-    Arma INT FOREIGN KEY REFERENCES Item (idItem),
-    Armadura INT FOREIGN KEY REFERENCES Item (idItem)
-  );
-
--- Tabela Habilidade
-CREATE TABLE
-  Habilidade (
-    idHabilidade INT PRIMARY KEY AUTO_INCREMENT,
-    custo INT NOT NULL,
-    alma VARCHAR(255) FOREIGN KEY REFERENCES Alma (Nome)
-  );
-
--- Tabela Genital
-CREATE TABLE
-  Genital (
-    idParte INT PRIMARY KEY,
-    Personagem INT FOREIGN KEY REFERENCES Personagem (idPersonagem),
-    tipoParte INT UNIQUE,
-    hpMaximo INT NOT NULL,
-    hitChance INT NOT NULL,
-    hpAtual INT NOT NULL
-  );
-
--- Tabela Cabeca
-CREATE TABLE
-  Cabeca (
-    idParte INT PRIMARY KEY,
-    Personagem INT FOREIGN KEY REFERENCES Personagem (idPersonagem),
-    tipoParte INT UNIQUE,
-    hpMaximo INT NOT NULL,
-    hitChance INT NOT NULL,
-    hpAtual INT NOT NULL
-  );
-
--- Tabela Olho
-CREATE TABLE
-  Olho (
-    idParte INT PRIMARY KEY,
-    Personagem INT FOREIGN KEY REFERENCES Personagem (idPersonagem),
-    tipoParte INT UNIQUE,
-    hpMaximo INT NOT NULL,
-    hitChance INT NOT NULL,
-    hpAtual INT NOT NULL
-  );
-
--- Tabela Braco
-CREATE TABLE
-  Braco (
-    idParte INT PRIMARY KEY,
-    Personagem INT FOREIGN KEY REFERENCES Personagem (idPersonagem),
-    tipoParte INT UNIQUE,
-    hpMaximo INT NOT NULL,
-    hitChance INT NOT NULL,
-    hpAtual INT NOT NULL
-  );
-
--- Tabela Torco
-CREATE TABLE
-  Torco (
-    idParte INT PRIMARY KEY,
-    Personagem INT FOREIGN KEY REFERENCES Personagem (idPersonagem),
-    tipoParte INT UNIQUE,
-    hpMaximo INT NOT NULL,
-    hitChance INT NOT NULL,
-    hpAtual INT NOT NULL
-  );
-
--- Tabela Perna
-CREATE TABLE
-  Perna (
-    idParte INT PRIMARY KEY,
-    Personagem INT FOREIGN KEY REFERENCES Personagem (idPersonagem),
-    tipoParte INT UNIQUE,
-    hpMaximo INT NOT NULL,
-    hitChance INT NOT NULL,
-    hpAtual INT NOT NULL
-  );
+CREATE TABLE ListaInventario(
+  idPj INT REFERENCES PersonagemJogavel(idPersonagem),
+  idItem SERIAL,
+  idInstItem SERIAL,
+  PRIMARY KEY (idPj),
+  FOREIGN KEY (idItem, idInstItem) REFERENCES InstanciaItem(idItem, idInst)
+);
