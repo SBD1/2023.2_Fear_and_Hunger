f5df7a727df4348c1d750eb58206e2b96f0bd9f1
Alexandre Beck <alexandrebeck2003@gmail.com>
2023-12-06 19:58:57 -0300

    Added regioes cards

--- game/FrontEnd/src/pages/Regiao/index.tsx
+++ game/FrontEnd/src/pages/Regiao/index.tsx
@@ -1,9 +1,34 @@
-import { WholePage, Container } from "./styles";
+import { useEffect, useState } from "react";
+import api from "../../api";
+import { Container, RegiaoContainer, WholePage } from "./styles";
+
+import { IRegiao } from "../../types";
 
 export default function Regiao() {
+  const [regioes, setRegioes] = useState<IRegiao[]>([]);
+  const getRegioes = async () => {
+    try {
+      const { data } = await api.get("/regiao");
+      setRegioes(data);
+    } catch (error) {
+      console.error("Erro ao obter personagens:", error);
+    }
+  };
+
+  useEffect(() => {
+    getRegioes();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log(regioes);
+
   return (
     <WholePage>
-      <Container></Container>
+      <Container>
+        {regioes?.map((regiao) => {
+          return <RegiaoContainer>{regiao.nomer}</RegiaoContainer>;
+        })}
+      </Container>
     </WholePage>
   );
 }
